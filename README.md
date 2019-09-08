be391dc759960539806a6f43730579c568652782
BuildTools <unconfigured@null.spigotmc.org>
2019-09-08 23:36:26 +0800

    frist commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RandomImage
+- 请给目录下的`index.html`写入权限！
+Linux 下的指令为`chmod 777 index.html`
+- 如果你准备在根目录下使用本程序，也就是说你把本程序的所有内容放在根目录下准备通过域名什么的直接访问的话。请删除文件`getImage.php`中的第八行的`/RandomImg`。
---
+++ getImage.php
@@ -0,0 +1,27 @@
+<?php
+if(time()-filemtime("index.html")>=60){
+	include('File.class.php');
+	$imgList = new File();
+	$info=$imgList->getFiles('images/',true);
+	$file = fopen("index.html","w");
+	fwrite($file,'<meta charset="UTF-8">'.'
+	<img onload="update()" src="'.'//'.$_SERVER['HTTP_HOST'].'/RandomImg/images/'.$info[rand(0,count($info)-1)].'" />'.
+	'<script>
+	function update()
+	{
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+		{
+			xmlhttp=new XMLHttpRequest();
+		}
+		else
+		{
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.open("GET","getImage.php","true");
+		xmlhttp.send();
+	}
+	</script>');
+	fclose($file);
+}
+?>
